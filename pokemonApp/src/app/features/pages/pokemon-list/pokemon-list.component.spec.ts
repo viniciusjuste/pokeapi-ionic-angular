d91db7d84b0cbad90ca290d7633c1deef04f4dfd
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-17 16:14:18 -0300

    feat: create list-pokemon component

---
+++ pokemonApp/src/app/features/pages/pokemon-list/pokemon-list.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { PokemonListComponent } from './pokemon-list.component';
+
+describe('PokemonListComponent', () => {
+  let component: PokemonListComponent;
+  let fixture: ComponentFixture<PokemonListComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PokemonListComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PokemonListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
