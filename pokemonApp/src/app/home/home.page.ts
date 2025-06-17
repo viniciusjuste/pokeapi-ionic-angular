5b84e722cce19b3ec862316b2e69b7116728fb70
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-17 20:14:00 -0300

    feat: calls the pokemon-list component

--- pokemonApp/src/app/home/home.page.ts
+++ pokemonApp/src/app/home/home.page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { PokemonListComponent } from "../features/pages/pokemon-list/pokemon-list.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonListComponent],
 })
 export class HomePage {
   constructor() {}
