f20002eadd5d3d4e6c722ff96231198fb44db0f6
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-17 17:11:54 -0300

    feat: created a function to get the image and ID of the Pokémon

--- pokemonApp/src/app/services/poke-api.service.ts
+++ pokemonApp/src/app/services/poke-api.service.ts
@@ -1,10 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
+  private baseUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
   constructor(private http: HttpClient) { }
 
@@ -14,7 +16,27 @@ export class PokeApiService {
    * @param offset the starting index of the Pokemon to return
    * @returns an observable of the Pokemon list
    */
-  getPokemonList(limit: number, offset: number) {
-    return this.http.get(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`);
+  getPokemonList(limit: number, offset: number): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}?limit=${limit}&offset=${offset}`);
+  }
+
+  /**
+   * Returns the id of a Pokemon given its PokeAPI url.
+   * @param pokemonUrl the PokeAPI url of the Pokemon
+   * @returns the id of the Pokemon
+   */
+  getPokemonId(pokemonUrl: string) {
+    const parts = pokemonUrl.split('/');
+    return parts[parts.length - 2];
+  }
+
+  /**
+   * Returns the image URL of a Pokemon given its PokeAPI url.
+   * @param pokemonUrl the PokeAPI url of the Pokemon
+   * @returns the URL of the Pokemon image
+   */
+  getPokemonImage(pokemonUrl: string) {
+    const id = this.getPokemonId(pokemonUrl);
+    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   }
 }
