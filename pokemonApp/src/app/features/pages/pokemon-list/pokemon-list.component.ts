024486986d41b21393d7722160e9fdaa1eff32b5
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-17 17:14:13 -0300

    feat: consume the service functions

--- pokemonApp/src/app/features/pages/pokemon-list/pokemon-list.component.ts
+++ pokemonApp/src/app/features/pages/pokemon-list/pokemon-list.component.ts
@@ -1,14 +1,51 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { PokeApiService } from 'src/app/services/poke-api.service';
 
 @Component({
   selector: 'app-pokemon-list',
+  imports: [IonicModule, CommonModule],
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.scss'],
 })
-export class PokemonListComponent  implements OnInit {
+export class PokemonListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pokeApiService: PokeApiService) { }
 
-  ngOnInit() {}
+  limit = 10;
+  offset = 0;
+  pokemonList: any[] = [];
+  loading = false;
 
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  getPokemonList(limit: number, offset: number) {
+    return this.pokeApiService.getPokemonList(limit, offset);
+  }
+
+  loadPokemons() {
+    this.loading = true;
+    this.getPokemonList(this.limit, this.offset).subscribe({
+      next: (response) => {
+        this.pokemonList = [...this.pokemonList, ...response.results];
+        this.loading = false;
+      },
+      error: (error) => {
+        console.error(error);
+        this.loading = false;
+      }
+    })
+  }
+
+ getPokemonImage(pokemonUrl: string) {
+  return this.pokeApiService.getPokemonImage(pokemonUrl);
+}
+
+  loadNextPage() {
+    this.offset += this.limit;
+    this.loadPokemons();
+  }
 }
