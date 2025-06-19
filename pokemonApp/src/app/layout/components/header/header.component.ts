97ed557100ebdccf6147c3dac8ee801b5231ec63
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-19 01:58:07 -0300

    create a toggle dropdown function

--- pokemonApp/src/app/layout/components/header/header.component.ts
+++ pokemonApp/src/app/layout/components/header/header.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent  implements OnInit {
+export class HeaderComponent implements OnInit {
+  dropdownIsOpen: boolean = false;
 
   constructor() { }
 
-  ngOnInit() {}
+  toggleDropdown() {
+    this.dropdownIsOpen = !this.dropdownIsOpen;
+  }
+
+  ngOnInit() { }
 
 }
