ffd037dce9f6a24d17895cf651246a3778c5150d
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-06-19 01:50:27 -0300

    feat: calls the header component on the home page

--- pokemonApp/src/app/home/home.page.ts
+++ pokemonApp/src/app/home/home.page.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { PokemonListComponent } from "../features/pages/pokemon-list/pokemon-list.component";
+import { HeaderComponent } from "../layout/components/header/header.component";
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonListComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, PokemonListComponent, HeaderComponent],
 })
 export class HomePage {
   constructor() {}
